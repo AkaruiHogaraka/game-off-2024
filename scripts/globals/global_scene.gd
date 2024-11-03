41f080ceced6aba5a970fd33c1c065bc7223bbcb
Akiro <88640273+AkaruiHogaraka@users.noreply.github.com>
2024-11-03 19:14:46 +1030

    Adds colour palette controller

--- scripts/globals/global_scene.gd
+++ scripts/globals/global_scene.gd
@@ -8,7 +8,7 @@ func change_scene(scene: SceneConnection) -> void:
 	
 	CurrentScene.queue_free()
 	CurrentScene = scene.scene
-	add_child(CurrentScene)
+	GlobalReference.Player.get_parent().add_child(CurrentScene)
 	
 	var position: Vector2 = scene.node.get_global_position()
 	GlobalReference.Player.set_global_position(Vector2(position.x, position.y))
