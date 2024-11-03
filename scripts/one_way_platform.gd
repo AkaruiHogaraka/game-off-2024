5e98ceed1309c2c38b8e7507bed85d2689c3aa6a
Akiro <88640273+AkaruiHogaraka@users.noreply.github.com>
2024-11-03 22:14:23 +1030

    Fixes one-way platform start state

--- scripts/one_way_platform.gd
+++ scripts/one_way_platform.gd
@@ -8,18 +8,17 @@ var process_platform: bool
 
 func _ready() -> void:
 	GlobalReference.Player.Input_Handler.DownJump.connect(_disable_collision)
+	process_platform = true
 
 func _physics_process(delta: float) -> void:
 	var disabled: bool = GlobalReference.Player.global_position.y >= platform_position.global_position.y
-	if disabled: collision.set_disabled(disabled)
-	
 	if not process_platform: return
 	collision.set_disabled(disabled)
 
 func _disable_collision() -> void:
 	collision.set_disabled(true)
 	process_platform = false
-	await get_tree().create_timer(0.1).timeout
+	await get_tree().create_timer(0.05).timeout
 	process_platform = true
 
 func _on_body_entered(body: Node2D) -> void:
