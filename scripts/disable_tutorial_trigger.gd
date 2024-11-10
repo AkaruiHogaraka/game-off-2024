d0b052ce9c0ac022ffd11fcabfa7445dc33dda3b
Akiro <88640273+AkaruiHogaraka@users.noreply.github.com>
2024-11-10 18:24:19 +1030

    Fixes crash with tutorial text
    Also adds tutorial 3 - Moving objects

--- scripts/disable_tutorial_trigger.gd
+++ scripts/disable_tutorial_trigger.gd
@@ -6,14 +6,16 @@ var active: bool = true
 var wait: bool
 
 func _physics_process(delta: float) -> void:
+	if tutorial == null: return
 	if is_all_areas_triggered() and not wait and active:
-		print("Trigged")
 		active = false
 		wait = true
 		tutorial.set_modulate(Color.RED)
 		await get_tree().create_timer(0.24).timeout
+		if tutorial == null: return
 		tutorial.set_modulate(Color.GREEN)
 		await get_tree().create_timer(0.24).timeout
+		if tutorial == null: return
 		tutorial.set_visible(active)
 		wait = false
 
@@ -27,4 +29,4 @@ func save_data() -> Dictionary:
 
 func load_data(data: Dictionary) -> void:
 	active = data["active"]
-	tutorial.set_visible(active)
+	if not active and tutorial != null: tutorial.queue_free()
--- scripts/walk_scene_connection.gd
+++ scripts/walk_scene_connection.gd
@@ -1,15 +1,13 @@
 extends Area2D
 
 var in_area: bool
-var enter_once: bool
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
 func _physics_process(delta: float) -> void:
-	if not in_area or enter_once: return
-	enter_once = true
+	if not in_area: return
 	GlobalScene.change_scene(get_parent().get_child(0))
 	
 func _on_body_entered(body: Node2D) -> void:
