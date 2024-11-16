692417eb6af0c753bc3559d7eb66e6241770ad29
Akiro <88640273+AkaruiHogaraka@users.noreply.github.com>
2024-11-16 14:09:39 +1030

    Forces cycle item to end on item pickup

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -19,12 +19,12 @@ func on_item_use() -> void:
 	
 	items[item_index].on_item_use()
 
-func on_cycle_items() -> void:
+func on_cycle_items(force_to_index: bool = false, to: int = 0) -> void:
 	if items.is_empty(): return
 	
 	if item_index >= 0: items[item_index].on_item_unequip()
 	
-	item_index = (item_index + 1) % items.size()
+	item_index = (item_index + 1) % items.size() if not force_to_index else to
 	
 	GlobalReference.Game.reality_inventory_ui.set_texture(items[item_index].ui_texture)
 	GlobalReference.Game.dream_inventory_ui.set_texture(items[item_index].ui_texture)
@@ -34,4 +34,4 @@ func on_cycle_items() -> void:
 func add_item_to_inventory(item: BaseInventoryItem) -> void:
 	items.append(item)
 	add_child(item)
-	on_cycle_items()
+	on_cycle_items(true, get_child_count() - 1)
