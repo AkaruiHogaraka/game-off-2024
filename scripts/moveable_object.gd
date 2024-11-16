03277ffd98ee9483f22ae96780c331e6501efaae
Akiro <88640273+AkaruiHogaraka@users.noreply.github.com>
2024-11-16 16:02:20 +1030

    Adds moveable object impact SFX

--- scripts/moveable_object.gd
+++ scripts/moveable_object.gd
@@ -8,38 +8,48 @@ extends BaseInteraction
 @export var right_ray: RayCast2D
 @export var ground_ray: RayCast2D
 
+@export var ground_impact_sfx: AudioStreamPlayer
+
 var holding_interaction: bool
 
 var distance_to_maintain: Vector2
 
 var fall_let_go_once: bool
+var impact_once: bool
 
 var gravity: float
+var initialise_delay: float
 
 func _initialise() -> void:
 	GlobalReference.Player.Input_Handler.InteractionLetGo.connect(_let_go_interaction)
+	initialise_delay = 0.1
 
 func _physics_process(delta: float) -> void:
 	if holding_interaction and not GlobalReference.Player.is_on_floor():
 		_let_go_interaction()
 		return
 	
+	if initialise_delay >= 0:
+		initialise_delay -= delta
+	
+	if _is_on_floor():
+		moveable_parent.global_position = round(moveable_parent.global_position)
+		
+		if impact_once: 
+			GlobalAudio.play_sfx(ground_impact_sfx)
+			impact_once = false
+	
 	if not _is_on_floor():
 		if not fall_let_go_once:
 			fall_let_go_once = true
 			_let_go_interaction()
+			if not call_deferred("_is_on_floor"):
+				
+				impact_once = true
 		
 		gravity += 4 * delta
 		
-		var collision_info: KinematicCollision2D
-		
-		if moveable_parent.test_move(moveable_parent.transform, Vector2.DOWN, collision_info): 
-			if ground_ray.is_colliding():
-				moveable_parent.global_position.y = ground_ray.get_collision_point().y
-			return
-		
 		moveable_parent.move_and_collide(Vector2.DOWN * gravity)
-		moveable_parent.global_position = round(moveable_parent.global_position)
 	
 	if not holding_interaction: return 
 	
@@ -84,7 +94,7 @@ func _let_go_interaction(override: bool = false) -> void:
 	moveable_parent.global_position = round(moveable_parent.global_position)
 	
 func _is_on_floor() -> bool:
-	if left_ray.is_colliding() or right_ray.is_colliding() or ground_ray.is_colliding(): return true
+	if left_ray.is_colliding() or right_ray.is_colliding() or ground_ray.is_colliding() or initialise_delay >= 0: return true
 	else: return false
 
 func save_data() -> Dictionary:
